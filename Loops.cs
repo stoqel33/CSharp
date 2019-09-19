1f985f9ad23a552319b9b6828e1da06cedfb2c47
stoqel33 <48477465+stoqel33@users.noreply.github.com>
2019-09-19 21:35:50 +0200

    Loops
    Loops excercises

---
+++ Loops.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace CSharp2
+{
+    public class Loops
+    {
+        public void Exercise1()
+        {
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i%3 == 0)
+                    count++;
+            }
+            Console.WriteLine("There are {0} numbers divisible by 3 between 1 and 100.", count);
+
+        }
+
+        public void Exercise2()
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
+
+        }
+
+        public void Exercise3()
+        {
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+            for (var i = 1; i <= number; i++)
+                factorial *= i;
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+
+        }
+
+        public void Exercise4()
+        {
+            var number = new Random().Next(1, 10);
+
+            //Console.WriteLine("Secret is " + number);
+            for (var i = 0; i < 4; i++)
+            {
+                Console.Write("Guess the secret number: ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+            }
+            Console.WriteLine("You lost!");
+
+        }
+
+        public void Exercise5()
+        {
+            Console.Write("Enter commoa separated numbers: ");
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
+            {
+                var number = Convert.ToInt32(str);
+                if (number > max)
+                    max = number;
+            }
+            Console.WriteLine("Max is " + max);
+
+        }
+    }
+}
