ad61c01e6b851a590e87a0034bea4b3a8344f0f2
stoqel33 <48477465+stoqel33@users.noreply.github.com>
2019-09-20 15:52:39 +0200

    Sortowanie bąbelkowe

---
+++ Bubble_Sort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort
+{
+    class Sortowanie
+    {
+        public void EnterData(int[] number)
+        {
+            Console.WriteLine("Numbers in your array: ");
+            for (int i = 0; i < number.Length; i++)
+            {
+                if(i < number.Length -1)
+                    Console.Write(number[i] + ", ");
+                else
+                    Console.Write(number[i] + ". ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Process(int[] number)
+        {
+            int temp;
+
+            for (int i = 1; i <= number.Length - 1; i++)
+            {
+                for (int j = number.Length - 1; j >= i; j--)
+                {
+                    if (number[j - 1] > number[j])
+                    {
+                        temp = number[j - 1];
+                        number[j - 1] = number[j];
+                        number[j] = temp;
+                    }
+                }
+            }
+
+        }
+
+        public void Show(int[] number)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sorted array: ");
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (i < number.Length - 1)
+                    Console.Write(number[i] + ", ");
+                else
+                    Console.Write(number[i] + ". ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 50, 202, -13, -1200, 0, 33 };
+
+            var bubbleSort  = new Sortowanie();
+            bubbleSort.EnterData(arr);
+            bubbleSort.Process(arr);
+            bubbleSort.Show(arr);
+        }
+    }
+}
