dd0202bf0fe96eb8496425323a25bf60c9d2c1b8
stoqel33 <48477465+stoqel33@users.noreply.github.com>
2019-09-23 13:19:45 +0200

    Stack LIFO

---
+++ StackExcercise.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(13);
+            stack.Push("adam");
+            stack.Push("33");
+            stack.Push(5);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new ArgumentException("The object is null!");
+            else
+                _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var numberOfObj = _list.Count;
+            if (numberOfObj < 1)
+                throw new ArgumentException("List is empty!");
+            else
+            {
+                var currentObj = _list[numberOfObj - 1];
+                _list.Remove(currentObj);
+
+                return currentObj;
+            }
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
