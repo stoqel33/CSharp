0e7c7c495036c6f0236bf6904f7b9abc9e92ffe3
stoqel33 <48477465+stoqel33@users.noreply.github.com>
2019-09-23 12:36:40 +0200

    Strings

---
+++ Strings.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    public class Strings
+    {
+        public void Exercise1()
+        {
+            Console.Write("Enter a few numbers (eg 1-2-3-4): ");
+            var input = Console.ReadLine();
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));                
+
+            numbers.Sort();
+
+            var isConsecutive = true;
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] != numbers[i - 1] + 1)
+                {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+
+            var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(message);
+        }
+
+        public void Exercise2()
+        {
+            Console.Write("Enter a few numbers (eg 1-2-3-4): ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+                return;
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+                numbers.Add(Convert.ToInt32(number));
+
+            var uniques = new List<int>();
+            var includesDuplicates = false;
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+                else
+                {
+                    includesDuplicates = true;
+                    break;
+                }
+            }
+
+            if (includesDuplicates)
+                Console.WriteLine("Duplicate");
+        }
+
+        public void Exercise3()
+        {
+            Console.Write("Enter time: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            var components = input.Split(':');
+            if (components.Length != 2)
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+            
+            try
+            {
+                var hour = Convert.ToInt32(components[0]);
+                var minute = Convert.ToInt32(components[1]);
+
+                if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+                    Console.WriteLine("Ok");
+                else
+                    Console.WriteLine("Invalid Time");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Time");
+            }
+        }
+
+        public void Exercise4()
+        {
+            Console.Write("Enter a few words: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+
+            Console.WriteLine(variableName);
+        }
+
+
+        public void Exercise5()
+        {
+            Console.Write("Enter a word: ");
+            var input = Console.ReadLine().ToLower();
+
+            var vowels = new List<char>(new char[] {'a', 'e', 'o', 'u', 'i'});
+            var vowelsCount = 0;
+            foreach (var character in input)
+            {
+                if (vowels.Contains(character))
+                    vowelsCount++;
+            }
+
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
