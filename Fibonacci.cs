8e07cd0454c2b41a4477ceb19f80701aa43e33fb
stoqel33 <48477465+stoqel33@users.noreply.github.com>
2019-09-23 12:30:16 +0200

    Fibonacci

---
+++ Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fibek2(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            return
+                Fibek2(n - 2) + Fibek2(n - 1);
+        }
+
+        public static void Fibek1(int len)
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.Write("{0} {1}", a, b);
+
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibek2(10));
+            Fibek1(9);
+        }
+    }
+
+}
